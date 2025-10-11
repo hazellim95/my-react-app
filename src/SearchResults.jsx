fb88b2c8e82d0440c5de13fde10f867b4f372395
Hazel Lim <hazellim2895@gmail.com>
2025-10-11 12:59:52 +0800

    Implemented API that fetches top 5 songs from my spotify account and displays them in SearchResults

--- src/SearchResults.jsx
+++ src/SearchResults.jsx
@@ -11,15 +11,60 @@ function SearchResults(props) {
     const [error, setError] = useState(null);
 
     
+
+    
     useEffect(() => {
+
+        const token = 'BQDcGrxS-vEZpqv1bAezTbD7SgjdUG48z2PwPu2MzkjZ5eLhXgpLmP05SdEKCCoZLnXS5IYy-ahT4HOs6P5-xSldhDufnGeKgytpHwsVge66Jz__K_hGSUdG-0P6WGtLJKANZJim6yadlRWe-BTU0FhD4os5QnGknn-17zOgtX9zxYz-5oCQNgdaRwFF2gkrS4ClBwCj2A96DLVgwZ_wUr7qSblE5Bq7J83K4_IDOnGdBs_kBl9fXhibiUM8atGO7H0G-mzMnNQ_rPMfetsP2wHBLRlhLp9DP0bgmITZ';
+
         const fetchData = async () => {
         try {
-            const response = await fetch('https://api.spotify.com');
+
+             async function fetchWebApi(endpoint, method, body) {
+                const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+                    headers: {
+                    Authorization: `Bearer ${token}`,
+                    },
+                    method,
+                    body:JSON.stringify(body)
+                });
+                //return await res.json();
+                return res;
+            }
+
+            // async function getTopTracks(){
+            //     // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+            //     return (await fetchWebApi(
+            //         'v1/me/top/tracks?time_range=long_term&limit=5', 'GET'
+            //     )).items;
+            // }
+
+            //const response = await fetch('https://api.spotify.com');
+            const response = await fetchWebApi(
+                    'v1/me/top/tracks?time_range=long_term&limit=5', 'GET')
+            
+            
             if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
             }
             const result = await response.json();
-            setData(result);
+            
+            console.log(result.items);
+            console.log(typeof result.items);
+            console.log(
+                result.items?.map(
+                    ({name, artists}) =>
+                    `${name} by ${artists.map(artist => artist.name).join(', ')}`
+                )
+            );
+
+            const resultList = result.items?.map(
+                    ({name, artists}) =>
+                    `${name} by ${artists.map(artist => artist.name).join(', ')}`
+                )
+
+
+            setData(resultList);
         } catch (error) {
             setError(error);
         } finally {
@@ -47,7 +92,7 @@ function SearchResults(props) {
         <>
             <p>Search results:</p>
             <div id="container">
-                <h1>Fetched Data:</h1>
+                <p>Fetched Data:</p>
                 {/* Render your data here */}
                 <pre>{JSON.stringify(data, null, 2)}</pre>
                 <div id="searchResults">
